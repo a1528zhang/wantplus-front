2dad008ae25806912eb0beac21fa9291620cae9e
a1528zhang <beingszhang@gmail.com>
2016-07-07 23:45:27 +0800

    add containers

---
+++ src/containers/app.js
@@ -0,0 +1,19 @@
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import Hello from '../components/Hello'
+import * as Actions from  '../actions/action'
+
+//将state.initCounter绑定到组件props的initCounter
+function mapStateToProps(state) {
+    return {
+        initCounter: state.initCounter
+    }
+}
+//将action中的所有方法绑定到props上,这样组件就有了绑定的方法
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(Actions, dispatch); //绑定action文件
+
+}
+
+//通过react-redux提供的connect方法将我们需要的state中的数据和actions中的方法绑定到props上
+export default connect(mapStateToProps, mapDispatchToProps)(Hello)
