3284819829164ce016df4c085e8b96ab0c50fe7f
a1528zhang <beingszhang@gmail.com>
2016-07-16 17:39:14 +0800

    little fix

--- src/components/commodity/commodityList.js
+++ src/components/commodity/commodityList.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes } from 'react'
 import { Router, Route, Link } from 'react-router'
 import { render } from 'react-dom'
 import CommodityBrief from "./commodityBrief"
+import CommodityDetail from './commodityDetail'
 
 class CommodityList extends Component {
     componentDidMount(){
