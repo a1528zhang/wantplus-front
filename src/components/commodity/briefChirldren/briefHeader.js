d5e392192a0a0427963dbf845a1a7c20d79ed819
wushuang <beingswu@gmail.com>
2016-07-16 17:48:03 +0800

    add briefchildreb

---
+++ src/components/commodity/briefChirldren/briefHeader.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react'
+import { render } from 'react-dom'
+
+class BriefHeader extends Component {
+  render(){
+    return (
+      <div className="want-brief-header">
+
+      </div>
+    )
+  }
+}
+
+export default BriefHeader;
--- src/components/commodity/commodityBrief.js
+++ src/components/commodity/commodityBrief.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { Router, Route, Link } from 'react-router'
 import { render } from 'react-dom'
 import Mask from '../public/mask'
+import BriefHeader from '../commodity/briefChirldren/briefHeader'
 
 class CommodityBrief extends Component {
     render(){
@@ -14,6 +15,7 @@ class CommodityBrief extends Component {
                       <img className = "want-commodity-img" src={data.commodityImgUrl}/>
                     </div>
                     <Mask />
+                    <BriefHeader />
                   {
                     //
                     //
