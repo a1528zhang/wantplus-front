a946e8c50bb643bb4d84819100581d019b1c6b0e
wushuang <beingswu@gmail.com>
2016-07-26 12:34:27 +0800

    shop layerout done

---
+++ src/components/commodity/briefChirldren/briefFooter.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react'
+import { render } from 'react-dom'
+
+
+
+class BriefFooter extends Component {
+    render(){
+        return (
+            <div className="want-brief-footer">
+
+            </div>
+        )
+    }
+}
+
+export default BriefFooter;
