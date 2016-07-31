f6fcf530c2b068f47eb7962b9a7039fd2b94a785
wushuang <beingswu@gmail.com>
2016-07-31 17:05:58 +0800

    breif done

--- src/components/commodity/commodityBrief.js
+++ src/components/commodity/commodityBrief.js
@@ -1,37 +1,83 @@
 import React, { Component, PropTypes } from 'react'
 import { Router, Route, Link } from 'react-router'
 import { render } from 'react-dom'
-//import Mask from '../public/mask'
-import BriefHeader from '../commodity/briefChirldren/briefHeader'
 import BriefFooter from '../commodity/briefChirldren/briefFooter'
 
 class CommodityBrief extends Component {
-    handleClicked(event){
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            maskState:"want-mask",
+            starState1:"want-brief-header-left1",
+            starState2:"want-brief-header-left2"
+        };
+    }
+
+    handleClicked(){
         const {data,showCommodityDetail,addCommodityUrl} = this.props;
         addCommodityUrl(data.commodityId);
-        showCommodityDetail(data,"block")
+        showCommodityDetail(data,"block");
     }
-    handleHover(event){
-        console.log(this);
-        $(event.target).css("opacity","0.5");
+
+    handleHover(){
+        this.setState({
+            maskState: "want-mask-hover",
+            starState1: "want-brief-header-left1-hover",
+            starState2:"want-brief-header-left2-hover"
+        });
     }
-    handleLeave(event){
-        console.log(this);
-        $(event.target).css("opacity","0");
+
+    handleLeave(){
+        this.setState({
+            maskState: "want-mask",
+            starState1: "want-brief-header-left1",
+            starState2:"want-brief-header-left2"
+        });
     }
+
     render(){
         const {data}=this.props;
+
+        var commodityMask = this.state.maskState;
+        var commodityShow1 = this.state.starState1;
+        var commodityShow2 = this.state.starState2;
+
         return (
-            <div className = "want-commodity-brief"  onClick={this.handleClicked.bind(this)}>
-                <div className = "want-commodity-outbox" onMouseEnter={this.handleHover.bind(this)} onMouseLeave={this.handleLeave.bind(this)}>
-                    <div className="want-commodity-midbox">
-                      <img className = "want-commodity-img" src={data.commodityImgUrl}/>
+            <div className = "want-commodity-brief"  >
+
+                <div className = "want-commodity-outbox">
+
+                    <div className="want-brief-header-right"  >
+                        <div className="want-brief-header-up">
+                            ￥{data.commodityPrice}
+                        </div>
                     </div>
-                    <div className="want-mask" >
-                        <div className="want-mask-inner">zhangliaaa</div>
+
+                    <div onMouseEnter={this.handleHover.bind(this)}
+                         onMouseLeave={this.handleLeave.bind(this)}
+                         onClick={this.handleClicked.bind(this)}>
+
+
+                        <div className={commodityShow1}>
+                            <img src="http://7xwfiz.com1.z0.glb.clouddn.com/like.svg"/>
+                        </div>
+                        <div className={commodityShow2}>
+                            <img src="http://7xwfiz.com1.z0.glb.clouddn.com/save.svg"/>
+                        </div>
+
+
+                        <div className="want-commodity-midbox" >
+                            <img className = "want-commodity-img" src={data.commodityImgUrl}/>
+                        </div>
+                        <div className={commodityMask} >
+                            <div className="want-mask-inner">
+                                zhangliaaa
+                            </div>
+                        </div>
+                        <BriefFooter />
                     </div>
-                    <BriefHeader header={data}/>
-                    <BriefFooter />
+
                 </div>
             </div>
         )
